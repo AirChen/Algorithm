81597ac157aa7aa1e5284d08f6ae5b2c72a282f7
Air chen <renzhichen2012@163.com>
2021-05-18 21:51:59 +0800

    Update math.rs

--- src/math.rs
+++ src/math.rs
@@ -116,3 +116,33 @@ pub fn log2(value: isize) -> i8 {
 
     table[idx as usize]
 }
+
+int divide(int dividend, int divisor) {        
+    long x = dividend, y = divisor;
+    bool isNeg = false;
+    if ((x > 0 && y < 0) || (x < 0 && y > 0)) isNeg = true;
+    if (x < 0) x = -x;
+    if (y < 0) y = -y;
+    long l = 0, r = x;
+    while (l < r) {
+        long mid = l + r + 1 >> 1;
+        if (mul(mid, y) <= x) {
+            l = mid;
+        } else {
+            r = mid - 1;
+        }
+    }
+    long ans = isNeg ? -l : l;
+    if (ans > pow(2, 31)-1 || ans < 0 - pow(2, 31)) return pow(2, 31)-1;
+    return (int)ans;
+}
+
+long mul(long a, long k) {
+    long ans = 0;
+    while (k > 0) {
+        if ((k & 1) == 1) ans += a;
+        k >>= 1;
+        a += a;
+    }
+    return ans;
+}
